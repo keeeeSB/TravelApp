dae7182e842f9ba0d4c59c25f5a9512fdb0f951b
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2025-01-29 09:12:29 +0900

    Userモデルにバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,11 @@ class User < ApplicationRecord
          :Confirmable
 
   mount_uploader :profile_image, ProfileImageUploader
+
+  validates :name, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email,    presence: true, length: { maximum: 255 },
+                       format: { with: VALID_EMAIL_REGEX },
+                       uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
 end
