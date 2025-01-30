301417bf0abf9af3460fe01ca801554e6c686ac8
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2025-01-30 11:57:46 +0900

    アカウント承認機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :Confirmable
+         :confirmable
 
   mount_uploader :profile_image, ProfileImageUploader
 
