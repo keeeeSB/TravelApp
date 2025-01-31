d45b422bc75d9f35a9e4d4f12d7d2324f53c1bee
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2025-01-31 13:12:56 +0900

    新規登録のテストの編集

--- spec/system/sign_ups_spec.rb
+++ spec/system/sign_ups_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "新規登録", type: :system do
   include ActiveJob::TestHelper
 
-  scenario "ユーザーは新規登録ができ、アカウント承認用のメールが送信される" do
+  scenario "ユーザーは新規登録ができ、アカウント承認メールが送信される" do
     visit root_path
     click_link "新規登録"
 
@@ -25,5 +25,20 @@ RSpec.describe "新規登録", type: :system do
       expect(mail.subject).to eq "メールアドレス確認メール"
       expect(mail.body).to match "メールアドレスの確認"
     end
+  end
+
+  scenario "アカウント承認メールのリンクをクリックすると、アカウントを有効化できる" do
+    user = User.create!(
+      name: "テストユーザー",
+      email: "test@example.com",
+      password: "password",
+      confirmation_token: "sample_token",
+      confirmation_sent_at: Time.current
+    )
+
+    visit user_confirmation_url(confirmation_token: user.confirmation_token)
+
+    expect(page).to have_content "メールアドレスが確認できました。"
+    expect(user.reload.confirmed?).to be true
   end
 end
