4ef97479d5322ea6b775b218f4ea5372cd72c56b
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2025-02-01 00:40:04 +0900

    Postモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_01_28_153437) do
+ActiveRecord::Schema[7.2].define(version: 2025_01_31_153622) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title", null: false
+    t.text "body", null: false
+    t.string "location", null: false
+    t.string "image"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", default: "", null: false
@@ -30,4 +41,6 @@ ActiveRecord::Schema[7.2].define(version: 2025_01_28_153437) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "posts", "users"
 end
