3fbf99b06da7c097a90920c63e9e1b4dca40fe75
桐山恵輔 <kiriyamakeisuke@kei-MacBook-Air.local>
2025-02-02 10:51:46 +0900

    Postモデルにバリデーションを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get "users/show"
   devise_for :users
 
-  resources :users, only: [:show] do
+  resources :users, only: [ :show ] do
     resources :posts
   end
 
